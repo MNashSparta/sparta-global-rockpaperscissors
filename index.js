ccbc4b140cc206cb6115a629330b1fec0bbc5468
Tech-A67 <mnash@spartaglobal.com>
2018-04-25 10:16:09 +0100

    tidy up

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Variables
-gameLoop = true;
-playerChoice = 0;
-computerChoice = 0;
-playerScore = 0;
-computerScore = 0;
+var gameLoop = true;
+var playerChoice = 0;
+var computerChoice = 0;
+var playerScore = 0;
+var computerScore = 0;
 
 function playerTurn() {
   playerChoice = parseInt(prompt("Pick your poison. (1) Rock (2) Paper (3) Scissors"));
@@ -15,20 +15,21 @@ function computerTurn() {
 
 function decideWinner() {
   if (playerChoice === computerChoice) {
-    console.log("W:"+playerScore+" L:"+computerScore)
-    gameLoop = confirm("It's a draw. Play again?");
+    gameLoop = confirm("It's a draw. W:" + playerScore + " L:" + computerScore + " \n Play again?");
+  } else if ((playerChoice === 0 && computerChoice === 1) || (playerChoice === 1 && computerChoice === 2) || (playerChoice === 2 && computerChoice === 0)) {
+    playerScore++;
+    gameLoop = confirm("You win! W:" + playerScore + " L:" + computerScore + " \n Play again?");
+  } else {
+    computerScore++;
+    gameLoop = confirm("You lost. W:" + playerScore + " L:" + computerScore + " \n Play again?");
   }
-  else if ((playerChoice === 0 && computerChoice === 1) || (playerChoice === 1 && computerChoice === 2) || (playerChoice === 2 && computerChoice === 0))
-  {
-    playerScore ++;
-    console.log("W:"+playerScore+" L:"+computerScore)
-    gameLoop = confirm("You win! Play again?");
-  }
-  else
-  {
-    computerScore ++;
-    console.log("W:"+playerScore+" L:"+computerScore)
-    gameLoop = confirm("You lost. Play again?");
+}
+
+function scoreCounter() {
+  var score = 0;
+  return function() {
+    score++;
+    return score;
   }
 }
 
